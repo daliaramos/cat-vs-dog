0741e8cbe276ba9e40605d96ccef05e4229ea42a
Dalia Ramos and Kayla Ondracek <dalia@>
2018-01-09 14:39:56 -0800

    add alligator to yell at animals

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,13 @@ $(document).ready(function() {
   $("#woof").click(function() {
     $("ul#cat").append("<li>Meow</li>");
   })
+
+  $("#rarr").click(function() {
+    $(this).before('<p><img src="img/Gator-2.jpg" alt="picture of an alligator" class="alligator-img"></p>')
+    $("ul#cat").append("<li>AHHHH</li>");
+    $("ul#dog").append("<li>AHHHH</li>");
+    $(".alligator-img").click(function() {
+      $(this).remove();
+    });
+  });
 });
